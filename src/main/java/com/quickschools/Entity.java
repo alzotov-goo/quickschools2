120e1331238243bbfc03463f90f6f294706d27aa
alzotov.goo <azotov.goo@gmail.com>
2020-04-04 13:14:08 +0800

    cleaned some code

--- src/main/java/com/quickschools/Entity.java
+++ src/main/java/com/quickschools/Entity.java
@@ -8,20 +8,18 @@ import static com.quickschools.Utils.*;
 public class Entity {
     private String name;
     //let's assume primary key is always present and is called 'id'
-    //private PrimaryKey primaryKey;
     private Set<Field> fields;
 
     public Entity(String name, String[] fieldNameList)
     {
         this.name=name;
-        //primaryKey = new PrimaryKey("id",this); //always id for simplicity
         fields = new HashSet<Field>();
         int i = 0;
         for(String fieldName: fieldNameList) //let's say field type doesn't matter and it's out of scope of this project
         {
             Field field = new Field(fieldName, this, name+up(fieldName));
             //for simplicity assumption made that future report builder will manipulate fieldNames in the format tableName+fieldName e.g
-            //if table is student and field is name - report reference will be unique name studentName
+            //if table is student and field is name - report reference will be unique studentName
             fields.add(field);
         }
 
@@ -30,10 +28,6 @@ public class Entity {
     public String getName() {
         return name;
     }
-
-    /*public PrimaryKey getPrimaryKey() {
-        return primaryKey;
-    }*/
 
     public Set<Field> getFields() {
         return fields;
